b31223f4ded56f08cc762e7d06fbacc97f429d82
Sébastien Geiser <SGeiser@precitrame.com>
2025-03-20 10:49:20 +0100

    simplify namespaces

--- NppCSharpPluginPack/PluginInfrastructure/Preference_h.cs
+++ NppCSharpPluginPack/PluginInfrastructure/Preference_h.cs
@@ -5,7 +5,7 @@
 // found at
 // https://github.com/notepad-plus-plus/notepad-plus-plus/blob/master/scintilla/include/Scintilla.iface
 
-namespace NppDemo.PluginInfrastructure
+namespace Kbg.NppPluginNET..PluginInfrastructure
 {
 	public enum Preference
 	{
